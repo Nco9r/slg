c02526113a3592186307a39ea6b2edddb3736be0
Nco9r <nicolas929@me.com>
2022-01-18 16:15:10 +0100

    push

--- pages/mentions-legales.vue
+++ pages/mentions-legales.vue
@@ -1,8 +1,14 @@
 <template>
+<div>
+     <div class="open">
+      <img src="~assets/img/svg/wave_header.svg" alt="" />
+    </div>
+
   <main>
       <hero-mentions/>
       <content-mention/>
   </main>
+</div>
 </template>
 
 <script>
@@ -16,5 +22,50 @@ export default {
 </script>
 
 <style>
+
+.open {
+  position: fixed;
+  inset: 0;
+  z-index: 1000;
+  background-color: var(--turquoise);
+  animation: bck 3s ease-in-out;
+  animation-fill-mode: forwards;
+}
+
+.open img {
+  margin-top: -80px;
+  opacity: 0;
+  animation: appear .6s ease-in-out;
+  animation-fill-mode: forwards;
+  animation-delay: 1.3s;
+   
+ 
+}
+
+@keyframes bck {
+  0% {
+    background-color: var(--bleu);
+    
+  }
+
+  60% {
+    background-color: var(--turquoise);
+    transform: translateY(0);
+  }
+   100% {
+    transform: translateY(115%);
+  }
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+    
+  }
+
+   100% {
+    opacity: 1;
+  }
+}
 
 </style>
